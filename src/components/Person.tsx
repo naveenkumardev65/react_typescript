1cb8b94b360374eed5a5873218ca09719a17e7e2
naveenkumardev65 <yuvannaveen65@gmail.com>
2024-03-17 21:19:55 +0530

    Reuse the type

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,10 +1,6 @@
 
-type PersonProps = {
-    name: {
-        first: string,
-        last: string
-    }
-}
+import { PersonProps } from "./Person.type";
+
 
 export default function Person(props: PersonProps) {
   return <div>{props?.name?.first} {props?.name?.last}</div>;
--- src/components/PersonList.tsx
+++ src/components/PersonList.tsx
@@ -1,8 +1,6 @@
+import { Name } from "./Person.type";
 type PersonListProps = {
-    names: {
-        first: string,
-        last: string
-    }[]
+    names: Name[]
 }
 
 export default function PersonList(props: PersonListProps) {
